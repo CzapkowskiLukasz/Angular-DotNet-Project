26c498370629d5699464d4126f5aa53ee0efb5fb
lukasz47 <77518@student.pb.edu.pl>
2021-10-21 12:51:23 +0200

    show me the dupa

--- MVC_Project_Api/MVC_Project/Controllers/WeatherForecastController.cs
+++ MVC_Project_Api/MVC_Project/Controllers/WeatherForecastController.cs
@@ -20,6 +20,7 @@ namespace MVC_Project.Controllers
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
+            Console.WriteLine("dupaaaaa");
             _logger = logger;
         }
 

61e3aa65a2ea8667e87f91bd8758837c52dc7ec9
lukasz47 <77518@student.pb.edu.pl>
2021-10-21 12:57:00 +0200

    change dupa to kutas

--- MVC_Project_Api/MVC_Project/Controllers/WeatherForecastController.cs
+++ MVC_Project_Api/MVC_Project/Controllers/WeatherForecastController.cs
@@ -20,7 +20,7 @@ namespace MVC_Project.Controllers
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            Console.WriteLine("dupaaaaa");
+            Console.WriteLine("kutassssss");
             _logger = logger;
         }
 

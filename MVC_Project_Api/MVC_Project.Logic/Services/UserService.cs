df66dc8d96796155fdd1ef59474a600a45d03094
Paweł <pawima00@gmail.com>
2021-11-04 13:17:45 +0100

    Added empty UserService

---
+++ MVC_Project_Api/MVC_Project.Logic/Services/UserService.cs
@@ -0,0 +1,60 @@
+﻿using MVC_Project.Domain;
+using MVC_Project.Domain.Entities;
+using MVC_Project.Logic.Interfaces;
+using MVC_Project.Logic.Requests;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MVC_Project.Logic.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly DataContext _dataContext;
+
+        public UserService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> LoginAsync(LoginRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> RegisterAsync(RegisterRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateAsync(int id, UpdateUserRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdatePasswordAsync(int id, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateRoleAsync(int id, string role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

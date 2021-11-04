edad93e43099f0a6f2ff2e9663ca4ba5585f9349
pawelwiszniewski <pawima00@gmail.com>
2021-11-04 21:46:03 +0100

    Add basic response classes

---
+++ MVC_Project_Api/MVC_Project.Logic/Responses/ErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_Project.Logic.Responses
+{
+    public class ErrorResponse
+    {
+        public string Message { get; set; }
+
+        public int ErrorCode { get; set; }
+    }
+}
---
+++ MVC_Project_Api/MVC_Project.Logic/Responses/HandleResult.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_Project.Logic.Responses
+{
+    public class HandleResult<T>
+    {
+        public T Response { get; set; }
+
+        public ErrorResponse ErrorResponse { get; set; }
+    }
+}

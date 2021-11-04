d2fcaaca9a6e476bd11608a6ca657eabe41f92ff
pawelwiszniewski <pawima00@gmail.com>
2021-11-04 18:46:19 +0100

    Add jwt authentication and move swagger configuration to extension method

---
+++ MVC_Project_Api/MVC_Project/Configurations/SecurityConfiguration.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using MVC_Project.Api.Middlewares;
+using MVC_Project.Logic.Settings;
+using System;
+using System.Text;
+
+namespace MVC_Project.Api.Configurations
+{
+    public static class SecurityConfiguration
+    {
+        public static void AddSecurity(this IServiceCollection services, IConfiguration configuration)
+        {
+            var swaggerSettings = GetSwaggerSettings(configuration);
+
+            var jwtSettings = new JwtSettings();
+            configuration.Bind(nameof(jwtSettings), jwtSettings);
+
+            var tokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                RequireExpirationTime = false,
+                ValidateLifetime = true
+            };
+
+            services.AddSingleton(tokenValidationParameters);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.SaveToken = true;
+                x.TokenValidationParameters = tokenValidationParameters;
+            });
+
+            services.AddAuthorization();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(swaggerSettings.Version, new OpenApiInfo { Title = swaggerSettings.Title, Version = swaggerSettings.Version });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "JWT Authorization header using the bearer scheme",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                    });
+            });
+        }
+
+        public static void UseSecurity(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            var swaggerSettings = GetSwaggerSettings(configuration);
+            var description = $"{swaggerSettings.Title} {swaggerSettings.Version}";
+
+            app.UseMiddleware<SwaggerAuthenticationMiddleware>();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint(swaggerSettings.UIEndpoint, description));
+        }
+
+        private static SwaggerSettings GetSwaggerSettings(IConfiguration configuration)
+        {
+            var swaggerSettings = new SwaggerSettings();
+            configuration.Bind(nameof(swaggerSettings), swaggerSettings);
+
+            return swaggerSettings;
+        }
+    }
+}
--- MVC_Project_Api/MVC_Project/Startup.cs
+++ MVC_Project_Api/MVC_Project/Startup.cs
@@ -1,22 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using MVC_Project.Api.Configurations;
 using MVC_Project.Domain;
 using MVC_Project.Domain.Entities;
 using MVC_Project.Logic.Interfaces;
 using MVC_Project.Logic.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MVC_Project
 {
@@ -42,10 +35,8 @@ namespace MVC_Project
             services.AddScoped<IUserService, UserService>();
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MVC_Project", Version = "v1" });
-            });
+
+            services.AddSecurity(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,9 +45,9 @@ namespace MVC_Project
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MVC_Project v1"));
             }
+
+            app.UseSecurity(Configuration);
 
             app.UseHttpsRedirection();
 

98d2ffe48d3090db24d67252ac49c515f756c333
Paweł <pawima00@gmail.com>
2021-11-04 17:55:37 +0100

    Create UserController wth register method

---
+++ MVC_Project_Api/MVC_Project/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_Project.Logic.Interfaces;
+using MVC_Project.Logic.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Project.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<string> Register(RegisterRequest request)
+        {
+            var result = await _userService.RegisterAsync(request);
+            return result;
+        }
+    }
+}

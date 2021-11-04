892ea04e3674040784d3f15ea59cfb938ef2a87a
pawelwiszniewski <pawima00@gmail.com>
2021-11-04 22:51:05 +0100

    Add constructor to ErrorResponse

--- MVC_Project_Api/MVC_Project.Logic/Responses/ErrorResponse.cs
+++ MVC_Project_Api/MVC_Project.Logic/Responses/ErrorResponse.cs
@@ -2,6 +2,12 @@
 {
     public class ErrorResponse
     {
+        public ErrorResponse(string message, int errorCode = 0)
+        {
+            Message = message;
+            ErrorCode = errorCode;
+        }
+
         public string Message { get; set; }
 
         public int ErrorCode { get; set; }

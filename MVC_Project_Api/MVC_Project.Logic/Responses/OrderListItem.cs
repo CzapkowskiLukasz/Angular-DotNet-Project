2c876052753e96ea9fb41d4dbcee1447a68716c8
pawelwiszniewski <pawima00@gmail.com>
2021-11-07 18:16:49 +0100

    Add response for get order list

---
+++ MVC_Project_Api/MVC_Project.Logic/Responses/OrderListItem.cs
@@ -0,0 +1,13 @@
+﻿using MVC_Project.Domain.Entities;
+using System;
+
+namespace MVC_Project.Logic.Responses
+{
+    public class OrderListItem
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public OrderStatus OrderStatus { get; set; }
+    }
+}

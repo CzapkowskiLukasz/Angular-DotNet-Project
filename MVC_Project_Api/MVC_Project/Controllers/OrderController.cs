c1cbe4510cbf61895036dbb57d6f7a4b51b04cf3
pawelwiszniewski <pawima00@gmail.com>
2021-11-07 20:48:21 +0100

    Add order controller with get order list method

---
+++ MVC_Project_Api/MVC_Project/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Project.Logic.Interfaces;
+using System.Threading.Tasks;
+
+namespace MVC_Project.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrderListAsync()
+        {
+            var result = await _orderService.GetOrderListAsync();
+
+            return Ok(result.Response);
+        }
+    }
+}

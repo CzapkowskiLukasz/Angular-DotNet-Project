77782688d6d45fc07daa1889c723888d17443f2f
pawelwiszniewski <pawima00@gmail.com>
2021-11-07 19:29:29 +0100

    add order mapping profile

---
+++ MVC_Project_Api/MVC_Project.Logic/MappingProfiles/OrderProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MVC_Project.Domain.Entities;
+using MVC_Project.Logic.Responses;
+using System.Collections.Generic;
+
+namespace MVC_Project.Logic.MappingProfiles
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Order, OrderListItem>();
+
+            CreateMap<List<Order>, GetOrderListResponse>()
+                .ForMember(dest => dest.Orders, opt =>
+                    opt.MapFrom(src => src));
+        }
+    }
+}

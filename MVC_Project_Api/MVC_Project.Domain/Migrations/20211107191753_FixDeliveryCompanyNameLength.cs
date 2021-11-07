7aa53bf9e733a1961a9e6721eeced0104bd5a9fc
pawelwiszniewski <pawima00@gmail.com>
2021-11-07 20:47:27 +0100

    Fix delivery company name length

---
+++ MVC_Project_Api/MVC_Project.Domain/Migrations/20211107191753_FixDeliveryCompanyNameLength.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC_Project.Domain.Migrations
+{
+    public partial class FixDeliveryCompanyNameLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "DeliveryCompany",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(1)",
+                oldUnicode: false,
+                oldMaxLength: 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "DeliveryCompany",
+                type: "varchar(1)",
+                unicode: false,
+                maxLength: 1,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldUnicode: false,
+                oldMaxLength: 20);
+        }
+    }
+}

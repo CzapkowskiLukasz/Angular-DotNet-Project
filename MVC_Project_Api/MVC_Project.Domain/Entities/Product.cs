b1a4aefc23b01a00f5df8dff56bead26fd4a83c6
pawelwiszniewski <pawima00@gmail.com>
2021-11-08 01:00:58 +0100

    Set some product columns nullable

--- MVC_Project_Api/MVC_Project.Domain/Entities/Product.cs
+++ MVC_Project_Api/MVC_Project.Domain/Entities/Product.cs
@@ -12,11 +12,11 @@ namespace MVC_Project.Domain.Entities
         public decimal Price { get; set; }
         public int WarehouseQuantity { get; set; }
         public int? ExpertId { get; set; }
-        public string Description { get; set; }
-        public int CategoryId { get; set; }
+        public string? Description { get; set; }
+        public int? CategoryId { get; set; }
         public int? DiscountId { get; set; }
         public DateTime CreateDate { get; set; }
-        public int ProducerId { get; set; }
+        public int? ProducerId { get; set; }
 
         public virtual Category Category { get; set; }
         public virtual Producer Producer { get; set; }

6896e22c02bdb7e4950413b383272ded7ebb18f1
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2021-11-12 18:32:47 +0100

    add register step 2

---
+++ mvc-project-angular/src/app/modules/user/register/register-step2/register-step2.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-register-step2',
+  templateUrl: './register-step2.component.html',
+  styleUrls: ['./register-step2.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class RegisterStep2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

0d8fac4fc101f18081a3e22f47ff325f3b47ac30
lukasz47 <77518@student.pb.edu.pl>
2021-11-13 20:54:44 +0100

    Update project

--- mvc-project-angular/src/app/app-routing.module.ts
+++ mvc-project-angular/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { RegisterStep1Component } from './modules/user/register/register-step1/register-step1.component';
 import { RegisterStep2Component } from './modules/user/register/register-step2/register-step2.component';
+import { RegisterStep3Component } from './modules/user/register/register-step3/register-step3.component';
+import { RegisterStep4Component } from './modules/user/register/register-step4/register-step4.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterStep1Component },
-  { path: 'register/step2', component: RegisterStep2Component }
+  { path: 'register/step2', component: RegisterStep2Component },
+  { path: 'register/step3', component: RegisterStep3Component },
+  { path: 'register/step4', component: RegisterStep4Component }
 ];
 
 @NgModule({

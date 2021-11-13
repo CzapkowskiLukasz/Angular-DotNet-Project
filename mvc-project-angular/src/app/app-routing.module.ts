ebe9a4fbf928e4dddbe28c555fb20aa6e6c65ad0
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2021-11-13 18:19:55 +0100

    Add routing and link to admin product list

--- mvc-project-angular/src/app/app-routing.module.ts
+++ mvc-project-angular/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './modules/admin/admin-dashboard/admin-dashboard.component';
+import { AdminProductListComponent } from './modules/admin/product/admin-product-list/admin-product-list.component';
 import { HomeComponent } from './modules/home/home.component';
 import { LoginComponent } from './modules/user/login/login/login.component';
 import { RegisterStep1Component } from './modules/user/register/register-step1/register-step1.component';
@@ -17,7 +18,8 @@ const routes: Routes = [
   { path: 'register/step2', component: RegisterStep2Component },
   { path: 'register/step3', component: RegisterStep3Component },
   { path: 'register/step4', component: RegisterStep4Component },
-  { path: 'admin/dashboard', component: AdminDashboardComponent }
+  { path: 'admin/dashboard', component: AdminDashboardComponent },
+  { path: 'admin/products', component: AdminProductListComponent }
 ];
 
 @NgModule({

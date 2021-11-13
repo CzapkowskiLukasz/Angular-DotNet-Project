e627c0596c99dbac153b0069195f79a53e2faaca
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2021-11-13 18:02:04 +0100

    Add admin dashboard

--- mvc-project-angular/src/app/app.module.ts
+++ mvc-project-angular/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RegisterStep2Component } from './modules/user/register/register-step2/r
 import { RegisterStep3Component } from './modules/user/register/register-step3/register-step3.component';
 import { RegisterStep4Component } from './modules/user/register/register-step4/register-step4.component';
 import { LoginComponent } from './modules/user/login/login/login.component';
+import { AdminDashboardComponent } from './modules/admin/admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { LoginComponent } from './modules/user/login/login/login.component';
     RegisterStep3Component,
     RegisterStep4Component,
     LoginComponent,
+    AdminDashboardComponent
   ],
   imports: [
     BrowserModule,

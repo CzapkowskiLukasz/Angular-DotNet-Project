e48b960ffe12cb6834c987f6bd6b6ecd291fd8b2
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2021-11-13 19:47:25 +0100

    add link to create product

--- mvc-project-angular/src/app/app-routing.module.ts
+++ mvc-project-angular/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './modules/admin/admin-dashboard/admin-dashboard.component';
+import { AdminProductCreateComponent } from './modules/admin/product/admin-product-create/admin-product-create.component';
 import { AdminProductListComponent } from './modules/admin/product/admin-product-list/admin-product-list.component';
 import { HomeComponent } from './modules/home/home.component';
 import { LoginComponent } from './modules/user/login/login/login.component';
@@ -19,7 +20,8 @@ const routes: Routes = [
   { path: 'register/step3', component: RegisterStep3Component },
   { path: 'register/step4', component: RegisterStep4Component },
   { path: 'admin/dashboard', component: AdminDashboardComponent },
-  { path: 'admin/products', component: AdminProductListComponent }
+  { path: 'admin/products', component: AdminProductListComponent },
+  { path: 'admin/products/new-product', component: AdminProductCreateComponent }
 ];
 
 @NgModule({

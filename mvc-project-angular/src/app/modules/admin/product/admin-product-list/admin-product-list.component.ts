59cb87ddbc417c7f7cd31853afddaf1332a5ebf0
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2021-11-13 18:42:57 +0100

    add sample products

--- mvc-project-angular/src/app/modules/admin/product/admin-product-list/admin-product-list.component.ts
+++ mvc-project-angular/src/app/modules/admin/product/admin-product-list/admin-product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductListItem } from 'src/app/shared/models/product-list-item';
 
 @Component({
   selector: 'app-admin-product-list',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductListComponent implements OnInit {
 
+  products: ProductListItem[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.products = [
+      {
+        productId: 1,
+        name: 'test1',
+        price: 23,
+        warehouseQuantity: 4
+      },
+      {
+        productId: 2,
+        name: 'test2',
+        price: 365,
+        warehouseQuantity: 100
+      }
+    ]
   }
 
 }

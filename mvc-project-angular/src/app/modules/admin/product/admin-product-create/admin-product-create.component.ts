f4b4839e256918de22a215432fba8eb99de932e4
pawelwiszniewski <pawima00@gmail.com>
2021-11-15 00:34:33 +0100

    Replace category select with FilteredDropdown component

--- mvc-project-angular/src/app/modules/admin/product/admin-product-create/admin-product-create.component.ts
+++ mvc-project-angular/src/app/modules/admin/product/admin-product-create/admin-product-create.component.ts
@@ -1,7 +1,6 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { fromEvent } from 'rxjs';
-import { Category } from 'src/app/shared/models/category';
+import { FilteredDropdownListItem } from 'src/app/shared/models/filtered-dropdown-list-item';
 
 @Component({
   selector: 'app-admin-product-create',
@@ -15,8 +14,7 @@ export class AdminProductCreateComponent implements OnInit {
   searchCategoryControl: FormControl;
   searchCategoryFilterText = '';
 
-  searchCategoryList: Category[] = [];
-  searchCategoryFilteredList: Category[] = [];
+  searchCategoryList: FilteredDropdownListItem[] = [];
 
   categoriesNotFetched = false;
 
@@ -24,21 +22,9 @@ export class AdminProductCreateComponent implements OnInit {
 
   dropdownShown = false;
 
-  @ViewChild('catInput', { static: true }) catInputEl: ElementRef;
-
-  @ViewChild('dropdownList', { static: true }) ddl: ElementRef;
-
   catInputSub;
 
-  private unlistenFocus: () => void;
-  private unlistenBlur: () => void;
-  private unlistenMouseOver: () => void;
-
-
-
-  constructor(
-    private fb: FormBuilder,
-    private renderer2: Renderer2) {
+  constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       name: [''],
       price: [''],
@@ -57,32 +43,6 @@ export class AdminProductCreateComponent implements OnInit {
 
   }
 
-  ngAfterViewInit() {
-    this.catInputSub = fromEvent(this.catInputEl.nativeElement, 'click')
-      .subscribe(r => this.dropdownToggle());
-
-    this.unlistenFocus = this.renderer2.listen(this.catInputEl.nativeElement, 'focus', () => {
-      // this.dropdownShown = true;
-      let el = this.ddl.nativeElement;
-      this.showDdl();
-      
-      this.unlistenMouseOver = this.renderer2.listen(el, 'mousedown', () => {
-        this.unlistenMouseOver();
-        this.unlistenBlur();
-      });
-      this.unlistenBlur = this.renderer2.listen(this.catInputEl.nativeElement, 'blur', () => {
-        this.dropdownShown = false;
-        this.unlistenBlur();
-        this.hideDdl();
-      });
-    });
-  }
-
-  ngOnDestroy() {
-    // this.catInputSub.unsubscribe();
-    this.unlistenFocus();
-  }
-
   submit(): void {
 
   }
@@ -90,68 +50,12 @@ export class AdminProductCreateComponent implements OnInit {
   private getSearchList() {
     this.searchCategoryList = [
       {
-        categoryId: 1,
-        name: 'cat1'
+        value: '1',
+        text: 'cat1'
       }, {
-        categoryId: 2,
-        name: 'cat2'
+        value: '2',
+        text: 'cat2'
       }
     ];
-
-    this.searchCategoryFilteredList = this.searchCategoryList;
-  }
-
-  searchCategoryFilterTextChange() {
-    const filterText = this.searchCategoryControl.value;
-
-    if (this.searchCategoryFilterText!.length > filterText.length) {
-      this.searchCategoryFilteredList = this.searchCategoryList;
-    }
-
-    this.searchCategoryFilterText = filterText;
-
-    this.searchCategoryFilteredList = this.searchCategoryFilteredList.filter(category => {
-      return this.categoryFilter(category, filterText);
-    })
-  }
-
-  dropdownSelectCategory(category: Category) {
-    this.searchCategoryControl.setValue(category.name);
-
-    this.selectedCategoryId = category.categoryId;
-
-    this.searchCategoryFilterTextChange()
-
-    // this.dropdownShown = false;
-    this.hideDdl();
-  }
-
-  dropdownToggle() {
-    this.dropdownShown = !this.dropdownShown;
-  }
-
-  private categoryFilter(category: Category, filterText: string): boolean {
-    const filterTextString = filterText + '';
-
-    const filterTerms = filterTextString.split(' ');
-    for (const filterTerm of filterTerms) {
-      if (!category.name.includes(filterTerm)) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  private showDdl() {
-    let el = this.ddl.nativeElement;
-    this.renderer2.removeClass(el, 'hide');
-    this.renderer2.addClass(el, 'show');
-  }
-
-  private hideDdl() {
-    let el = this.ddl.nativeElement;
-    this.renderer2.removeClass(el, 'show');
-    this.renderer2.addClass(el, 'hide');
   }
 }

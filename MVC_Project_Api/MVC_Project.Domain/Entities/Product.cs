bb4c4fdabe1b6524aa009575f57fefd0f9f0ce8b
pawelwiszniewski <pawima00@gmail.com>
2021-11-25 11:44:30 +0100

    add SoldCount to Product

--- MVC_Project_Api/MVC_Project.Domain/Entities/Product.cs
+++ MVC_Project_Api/MVC_Project.Domain/Entities/Product.cs
@@ -17,6 +17,7 @@ namespace MVC_Project.Domain.Entities
         public int? DiscountId { get; set; }
         public DateTime CreateDate { get; set; }
         public int? ProducerId { get; set; }
+        public int SoldCount { get; set; }
 
         public virtual Category Category { get; set; }
         public virtual User Expert { get; set; }

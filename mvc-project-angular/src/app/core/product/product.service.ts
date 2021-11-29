fa0d7fa999bf697affec4caae1d5b00674847022
pawelwiszniewski <pawima00@gmail.com>
2021-11-29 19:58:39 +0100

    add product service with getBestsellers method

---
+++ mvc-project-angular/src/app/core/product/product.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SlideItem } from 'src/app/shared/models/slide-item';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  baseUrl = `${environment.api_url}/product`;
+
+  constructor(private http: HttpClient) { }
+
+  getBestsellers(count): Observable<any> {
+    return this.http.get<SlideItem[]>(this.baseUrl + '/bestsellers/' + count);
+  }
+}

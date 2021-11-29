edcb37d41621c4a9d1dac517af32e39ad61a2bff
pawelwiszniewski <pawima00@gmail.com>
2021-11-29 19:59:18 +0100

    add fetchnig bestsellers in home component

--- mvc-project-angular/src/app/modules/home/home.component.ts
+++ mvc-project-angular/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ProductService } from 'src/app/core/product/product.service';
 import { SlideItem } from 'src/app/shared/models/slide-item';
 
 @Component({
@@ -12,35 +13,40 @@ export class HomeComponent implements OnInit {
   count: number;
   showCart: boolean;
 
-  testItems: SlideItem[] = [{
-    productId: 1,
-    name: 'ziółko',
-    isPricePerItem: true,
-    price: 420
-  },
-  {
-    productId: 2,
-    name: 'ziółko 2',
-    isPricePerItem: true,
-    price: 214
-  }, {
-    productId: 3,
-    name: 'ziółko 3',
-    isPricePerItem: true,
-    price: 45
-  }, {
-    productId: 4,
-    name: 'ziółko 4',
-    isPricePerItem: true,
-    price: 90
-  }];
+  // testItems: SlideItem[] = [{
+  //   productId: 1,
+  //   name: 'ziółko',
+  //   isPricePerItem: true,
+  //   price: 420
+  // },
+  // {
+  //   productId: 2,
+  //   name: 'ziółko 2',
+  //   isPricePerItem: true,
+  //   price: 214
+  // }, {
+  //   productId: 3,
+  //   name: 'ziółko 3',
+  //   isPricePerItem: true,
+  //   price: 45
+  // }, {
+  //   productId: 4,
+  //   name: 'ziółko 4',
+  //   isPricePerItem: true,
+  //   price: 90
+  // }];
 
-  constructor() {
+  bestsellers: SlideItem[] = [];
+
+  constructor(private productService: ProductService) {
     this.count = 0;
     this.showCart = false;
   }
 
   ngOnInit(): void {
+    this.productService.getBestsellers(10).subscribe(result => {
+      this.bestsellers = result.bestsellers
+    });
   }
 
   increse() {

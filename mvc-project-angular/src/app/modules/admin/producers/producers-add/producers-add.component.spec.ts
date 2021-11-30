4e05811b6cf6fae4b1b0c7c03973c8e2c636acbd
lukasz47 <77518@student.pb.edu.pl>
2021-11-30 23:28:24 +0100

    Add creating producer form

---
+++ mvc-project-angular/src/app/modules/admin/producers/producers-add/producers-add.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProducersAddComponent } from './producers-add.component';
+
+describe('ProducersAddComponent', () => {
+  let component: ProducersAddComponent;
+  let fixture: ComponentFixture<ProducersAddComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProducersAddComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProducersAddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

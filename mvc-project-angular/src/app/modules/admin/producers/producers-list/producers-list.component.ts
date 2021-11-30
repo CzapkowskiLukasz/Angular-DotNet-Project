1fe92296ad94775294e90ad24ff7bab0cbdd73a1
lukasz47 <77518@student.pb.edu.pl>
2021-11-30 14:46:54 +0100

    Add bookmarks buttons, add producers bookmark

---
+++ mvc-project-angular/src/app/modules/admin/producers/producers-list/producers-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-producers-list',
+  templateUrl: './producers-list.component.html',
+  styleUrls: ['./producers-list.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class ProducersListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

543e8dcd50b606814914cf319dddd3d9848ebdbf
pawelwiszniewski <pawima00@gmail.com>
2021-12-01 20:59:24 +0100

    Add get producers response and mapping

---
+++ MVC_Project_Api/MVC_Project.Logic/Admin/Responses/AdminGetProducerListResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MVC_Project.Logic.Admin.Responses
+{
+    public class AdminGetProducerListResponse
+    {
+        public List<AdminProducerListItem> Producers { get; set; }
+    }
+}

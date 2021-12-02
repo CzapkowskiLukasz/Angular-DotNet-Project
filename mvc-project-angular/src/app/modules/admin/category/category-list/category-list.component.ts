074d45bc87b18954a4a1f1bc37fd90d75b70e387
lukasz47 <77518@student.pb.edu.pl>
2021-12-02 11:58:45 +0100

    Add categories list component

---
+++ mvc-project-angular/src/app/modules/admin/category/category-list/category-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CategoryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

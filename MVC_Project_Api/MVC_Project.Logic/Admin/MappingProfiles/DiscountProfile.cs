7e49e2743745864cb73d508a04171fe867f4a502
pawelwiszniewski <pawima00@gmail.com>
2021-12-02 16:57:51 +0100

    Add admin get discount list response and mapping

---
+++ MVC_Project_Api/MVC_Project.Logic/Admin/MappingProfiles/DiscountProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MVC_Project.Domain.Entities;
+using MVC_Project.Logic.Admin.Responses;
+using System.Collections.Generic;
+
+namespace MVC_Project.Logic.Global.MappingProfiles
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Discount, AdminDiscountListItem>();
+
+            CreateMap<List<Discount>, AdminGetDiscountListResponse>()
+                .ForMember(dest => dest.Discounts, opt =>
+                   opt.MapFrom(src => src));
+        }
+    }
+}
---
+++ MVC_Project_Api/MVC_Project.Logic/Admin/Responses/AdminDiscountListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MVC_Project.Logic.Admin.Responses
+{
+    public class AdminDiscountListItem
+    {
+        public int DiscountId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Percent { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}

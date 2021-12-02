eb309b4ccf7dd9d0d9447d7b58440c1c54bbaa0e
pawelwiszniewski <pawima00@gmail.com>
2021-12-02 09:34:17 +0100

    Add get category list response and mapping

---
+++ MVC_Project_Api/MVC_Project.Logic/Admin/MappingProfiles/CategoryProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MVC_Project.Domain.Entities;
+using MVC_Project.Logic.Admin.Responses;
+using System.Collections.Generic;
+
+namespace MVC_Project.Logic.Global.MappingProfiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, AdminCategoryListItem>();
+
+            CreateMap<List<Category>, AdminGetCategoryListResponse>()
+                .ForMember(dest => dest.Categories, opt =>
+                   opt.MapFrom(src => src));
+        }
+    }
+}

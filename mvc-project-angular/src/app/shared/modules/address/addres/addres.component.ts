7d0c2780fb5f7cc02b07b011856cb7853b052cc3
lukasz47 <77518@student.pb.edu.pl>
2021-12-03 23:31:33 +0100

    Correct user profile

---
+++ mvc-project-angular/src/app/shared/modules/address/addres/addres.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-addres',
+  templateUrl: './addres.component.html',
+  styleUrls: ['./addres.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AddresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

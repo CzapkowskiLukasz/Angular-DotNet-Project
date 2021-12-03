04d43ae1272a85c44792a08bfadb845f710d1327
pawelwiszniewski <pawima00@gmail.com>
2021-12-04 00:10:54 +0100

    Add logic in producer add component

--- mvc-project-angular/src/app/modules/admin/producers/producers-add/producers-add.component.ts
+++ mvc-project-angular/src/app/modules/admin/producers/producers-add/producers-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, EventEmitter, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CountryService } from 'src/app/core/country/country.service';
+import { ProducerService } from 'src/app/core/producer/producer.service';
+import { FilteredDropdownListItem } from 'src/app/shared/models/filtered-dropdown-list-item';
 
 @Component({
   selector: 'app-producers-add',
@@ -8,14 +12,34 @@ import { Component, EventEmitter, OnInit, Output, ViewEncapsulation } from '@ang
 })
 export class ProducersAddComponent implements OnInit {
 
+  @Output() createProducerEvent = new EventEmitter();
+
   @Output() cancelEvent = new EventEmitter();
 
-  constructor() { }
+  form: FormGroup;
+
+  countryId;
+  countryList: FilteredDropdownListItem[] = [];
+
+  isCountriesLoaded: boolean = false;
+
+  constructor(private fb: FormBuilder,
+    private countryService: CountryService,
+    private producerService: ProducerService) {
+    this.form = fb.group({
+      name: [''],
+      country: ['']
+    });
+  }
 
   ngOnInit(): void {
   }
 
   cancel() {
     this.cancelEvent.emit();
+  }
+
+  private fetchCountries(){
+    this.countryService.get
   }
 }

783c4eb842aa41ccada5792c4a56fb0c56251b9d
pawelwiszniewski <pawima00@gmail.com>
2021-12-03 21:26:45 +0100

    Add get country list response and mapping

---
+++ MVC_Project_Api/MVC_Project.Logic/Admin/MappingProfiles/CountryProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MVC_Project.Domain.Entities;
+using MVC_Project.Logic.Admin.Responses;
+using System.Collections.Generic;
+
+namespace MVC_Project.Logic.Global.MappingProfiles
+{
+    public class CountryProfile : Profile
+    {
+        public CountryProfile()
+        {
+            CreateMap<Country, AdminCountryListItem>()
+                .ForMember(dest => dest.ContinentName, opt =>
+                   opt.MapFrom(src => src.Continent.Name));
+
+            CreateMap<List<Country>, AdminGetCountryListResponse>()
+                .ForMember(dest => dest.Countries, opt =>
+                   opt.MapFrom(src => src));
+        }
+    }
+}
---
+++ MVC_Project_Api/MVC_Project.Logic/Admin/Responses/AdminCountryListItem.cs
@@ -0,0 +1,13 @@
+﻿namespace MVC_Project.Logic.Admin.Responses
+{
+    public class AdminCountryListItem
+    {
+        public int CountryId { get; set; }
+    
+        public string Name { get; set; }
+        
+        public int ContinentId { get; set; }
+        
+        public string ContinentName { get; set; }
+    }
+}

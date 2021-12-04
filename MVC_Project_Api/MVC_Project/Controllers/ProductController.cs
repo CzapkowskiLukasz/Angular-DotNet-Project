39de61da8c987fca458e9a4c24f2a209a8b304f3
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2021-12-04 20:03:09 +0100

    Add get product list by order in product controller

--- MVC_Project_Api/MVC_Project/Controllers/ProductController.cs
+++ MVC_Project_Api/MVC_Project/Controllers/ProductController.cs
@@ -70,6 +70,14 @@ namespace MVC_Project.Controllers
             }
         }
 
+        [HttpGet("product/by-order/{orderId}")]
+        public async Task<IActionResult> GetProductListByOrderAsync([FromRoute] int orderId)
+        {
+            var result = await _productService.GetListByOrder(orderId);
+
+            return Ok(result.Response);
+        }
+
         [HttpGet("bestsellers/{count}")]
         public async Task<IActionResult> GetBestsellersAsync([FromRoute] int count)
         {

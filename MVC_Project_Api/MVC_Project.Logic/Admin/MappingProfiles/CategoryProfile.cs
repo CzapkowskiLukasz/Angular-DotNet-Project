230647a1aa59bd98effe19b2be50671777274ac4
pawelwiszniewski <pawima00@gmail.com>
2021-12-06 22:17:21 +0100

    Add request, response and mapping for add category

--- MVC_Project_Api/MVC_Project.Logic/Admin/MappingProfiles/CategoryProfile.cs
+++ MVC_Project_Api/MVC_Project.Logic/Admin/MappingProfiles/CategoryProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MVC_Project.Domain.Entities;
+using MVC_Project.Logic.Admin.Requests;
 using MVC_Project.Logic.Admin.Responses;
 using System.Collections.Generic;
 
@@ -9,11 +10,16 @@ namespace MVC_Project.Logic.Global.MappingProfiles
     {
         public CategoryProfile()
         {
+            // Get dropdown list
+
             CreateMap<Category, AdminCategoryDropdownListItem>();
 
             CreateMap<List<Category>, AdminGetCategoryDropdownListResponse>()
                 .ForMember(dest => dest.Categories, opt =>
                    opt.MapFrom(src => src));
+
+
+            // Get list
 
             CreateMap<Category, AdminCategoryListItem>()
                 .ForMember(dest => dest.ParentId, opt =>
@@ -24,6 +30,18 @@ namespace MVC_Project.Logic.Global.MappingProfiles
             CreateMap<List<Category>, AdminGetCategoryListResponse>()
                 .ForMember(dest => dest.Categories, opt =>
                    opt.MapFrom(src => src));
+
+
+            // Add category
+
+            CreateMap<AddCategoryRequest, Category>()
+                .ForMember(dest => dest.ParentCategoryId, opt =>
+                   opt.MapFrom(src => ConvertParentId(src.ParentId)));
+
+            CreateMap<Category, AddCategoryResponse>();
         }
+
+        private int? ConvertParentId(int parentId) =>
+            parentId == 0 ? null : parentId;
     }
 }

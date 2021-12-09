e67fa94ae5f906d79b838e489c74c10fe811ed48
pawelwiszniewski <pawima00@gmail.com>
2021-12-09 21:56:54 +0100

    Add update in angular country service

--- mvc-project-angular/src/app/core/country/country.service.ts
+++ mvc-project-angular/src/app/core/country/country.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { getPlatform, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CountryListItem } from 'src/app/shared/models/country-list-item';
 import { environment } from 'src/environments/environment';
@@ -10,6 +10,8 @@ import { environment } from 'src/environments/environment';
 export class CountryService {
 
   baseAdminUrl = `${environment.api_url}/admin/country`;
+
+  baseGlobalUrl = `${environment.api_url}/global/country`;
 
   constructor(private http: HttpClient) { }
 
@@ -23,5 +25,13 @@ export class CountryService {
 
   add(newCountry): Observable<boolean> {
     return this.http.post<boolean>(this.baseAdminUrl, newCountry);
+  }
+
+  getById(countryId): Observable<any> {
+    return this.http.get<any>(this.baseGlobalUrl + '/' + countryId);
+  }
+
+  update(country): Observable<any> {
+    return this.http.put<any>(this.baseAdminUrl, country);
   }
 }

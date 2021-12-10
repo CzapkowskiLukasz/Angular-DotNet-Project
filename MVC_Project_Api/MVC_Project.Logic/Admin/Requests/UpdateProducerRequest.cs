52edce4083d6608379b9b0b81469af5f6c7b22a5
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2021-12-10 21:06:21 +0100

    Add update producer request, response and mapping

---
+++ MVC_Project_Api/MVC_Project.Logic/Admin/Requests/UpdateProducerRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_Project.Logic.Admin.Requests
+{
+    public class UpdateProducerRequest
+    {
+        public int ProducerId { get; set; }
+
+        public string Name { get; set; }
+
+        public int CountryId { get; set; }
+    }
+}

9a9ae7bdf97e98de9aee17027801236879085359
lukasz47 <77518@student.pb.edu.pl>
2021-12-10 16:03:48 +0100

    backup

--- mvc-project-angular/src/app/modules/product/product-details/product-details.component.ts
+++ mvc-project-angular/src/app/modules/product/product-details/product-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { ProductService } from 'src/app/core/product/product.service';
+import { ProductDetails } from 'src/app/shared/models/product';
 
 @Component({
   selector: 'app-product-details',
@@ -8,9 +13,25 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  id$
 
-  ngOnInit(): void {
+  dupa
+
+  product: ProductDetails
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) {
   }
 
+  ngOnInit(): void {
+    this.getProduct(5)
+  }
+
+  getProduct(id) {
+    this.productService.getDetails(+id).subscribe(result => {
+      this.product.name = result.name
+      this.product.price = result.price
+      this.product.count = result.count
+      this.product.producerName = result.producerName
+    });
+  }
 }
---
+++ mvc-project-angular/src/app/shared/models/product.ts
@@ -0,0 +1,8 @@
+export interface ProductDetails {
+    productId: number,
+    name: string,
+    price: number,
+    count: Date,
+    categoryName: string,
+    producerName: string
+}

7d31ea1f05375e214ceb0defa3e9c5ad46e5e13a
Paweł Wiszniewski <pawel.wiszniewski20@gmail.com>
2021-12-15 22:22:17 +0100

    Add value subscribtion in register step 1

--- mvc-project-angular/src/app/modules/user/register/register-step1/register-step1.component.ts
+++ mvc-project-angular/src/app/modules/user/register/register-step1/register-step1.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ComponentConnectionService } from 'src/app/core/componentConnection/component-connection.service';
+import { RegisterRequest } from 'src/app/shared/models/register-request';
 
 @Component({
   selector: 'app-register-step1',
@@ -6,25 +9,39 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./register-step1.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class RegisterStep1Component implements OnInit {
+export class RegisterStep1Component implements OnInit, OnDestroy {
 
   isEnglish: boolean;
   isPolish: boolean;
 
-  constructor() {
-    this.isEnglish = false;
-    this.isPolish = false;
-   }
+  registerRequest: RegisterRequest;
+
+  valueSubscribtion: Subscription;
+
+  constructor(private componentConnection: ComponentConnectionService) {
+  }
 
   ngOnInit(): void {
-  }
-  
-  selectEnglish(){
-  this.isEnglish = true
-  this.isPolish = false
+    this.isEnglish = false;
+    this.isPolish = false;
+
+    this.valueSubscribtion=this.componentConnection.lastValue.subscribe(obj=>{
+      if(obj.key=='registerRequest'){
+        this.registerRequest=obj.value;
+      }
+    })
   }
 
-  selectPolish(){
+  ngOnDestroy() {
+    this.valueSubscribtion.unsubscribe();
+  }
+
+  selectEnglish() {
+    this.isEnglish = true
+    this.isPolish = false
+  }
+
+  selectPolish() {
     this.isPolish = true
     this.isEnglish = false
   }

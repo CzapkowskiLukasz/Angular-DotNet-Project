576a8c08c89bbd89183437f891d585f238fe67e9
Paweł Wiszniewski <pawel.wiszniewski20@gmail.com>
2021-12-15 20:31:52 +0100

    Add cookie for authorization data

--- mvc-project-angular/src/app/core/user/user.service.ts
+++ mvc-project-angular/src/app/core/user/user.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { UserListItem } from 'src/app/shared/models/user-list-item';
 import { environment } from 'src/environments/environment';
 
@@ -13,14 +15,20 @@ export class UserService {
 
   baseGlobalUrl = `${environment.api_url}/user`;
 
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private cookieService: CookieService) { }
 
   getAdminList(): Observable<any> {
     return this.http.get<UserListItem[]>(this.baseAdminUrl);
   }
 
   login(request): Observable<any> {
-    return this.http.post<any>(this.baseGlobalUrl + '/login', request);
+    return this.http.post<any>(this.baseGlobalUrl + '/login', request)
+      .pipe(
+        tap(result => {
+          this.cookieService.set('jwt-authorization', 'true');
+          this.cookieService.set('jwt-token', result.token);
+        })
+      );
   }
 }

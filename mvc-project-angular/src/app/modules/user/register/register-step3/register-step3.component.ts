48bac820d79c40eafbe64767cc95232da1afcf62
Paweł Wiszniewski <pawel.wiszniewski20@gmail.com>
2021-12-16 03:15:03 +0100

    Add form group and store user name and surname in register step 3

--- mvc-project-angular/src/app/modules/user/register/register-step3/register-step3.component.ts
+++ mvc-project-angular/src/app/modules/user/register/register-step3/register-step3.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ComponentConnectionService } from 'src/app/core/componentConnection/component-connection.service';
+import { RegisterBase } from '../register-base';
 
 @Component({
   selector: 'app-register-step3',
@@ -6,11 +10,57 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./register-step3.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class RegisterStep3Component implements OnInit {
+export class RegisterStep3Component extends RegisterBase {
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit(): void {
+  constructor(protected componentConnection: ComponentConnectionService,
+    protected router: Router,
+    private fb: FormBuilder) {
+    super(componentConnection, router);
   }
 
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      street: ['', Validators.required],
+      houseNumber: ['', Validators.required],
+      code: ['', Validators.required],
+      city: ['', Validators.required]
+    });
+
+    super.ngOnInit();
+  }
+
+  ngOnDestroy(): void {
+    const formData = this.form.value;
+    this.registerRequest.name=formData.name;
+    this.registerRequest.surname=formData.surname;
+
+    super.ngOnDestroy();
+  }
+
+  protected incomingIsValid(): boolean {
+    return this.isNotEmpty(this.registerRequest)
+      && this.isNotEmpty(this.registerRequest.languageId)
+      && this.isNotEmpty(this.registerRequest.email)
+      && this.isNotEmpty(this.registerRequest.password);
+  }
+
+  protected outcomingIsValid(): boolean {
+    return this.form.valid;
+  }
+
+  protected prepareView() {
+    this.form.patchValue(this.registerRequest);
+  }
+
+  protected invalidIncomingAction() {
+    this.resetRegister();
+  }
+
+  protected invalidOutcomingAction() {
+    console.log(this.form.status);
+  }
 }

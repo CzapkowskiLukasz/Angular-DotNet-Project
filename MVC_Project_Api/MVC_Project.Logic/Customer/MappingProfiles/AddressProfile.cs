00ecc4fcec784616ecc50784d74f9f9a9bd30063
Paweł Wiszniewski <pawel.wiszniewski20@gmail.com>
2021-12-16 10:31:53 +0100

    Add add address request, response and mapping

---
+++ MVC_Project_Api/MVC_Project.Logic/Customer/MappingProfiles/AddressProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MVC_Project.Domain.Entities;
+using MVC_Project.Logic.Customer.Requests;
+
+namespace MVC_Project.Logic.Customer.MappingProfiles
+{
+    public class AddressProfile : Profile
+    {
+        public AddressProfile()
+        {
+            CreateMap<AddAddressRequest, Address>()
+                .ForMember(dest => dest.BuildingNumber, opt =>
+                    opt.MapFrom(src => src.HouseNumber))
+                .ForMember(dest => dest.ZipCode, opt =>
+                    opt.MapFrom(src => src.Code));
+        }
+    }
+}
---
+++ MVC_Project_Api/MVC_Project.Logic/Customer/Responses/AddAddressResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace MVC_Project.Logic.Customer.Responses
+{
+    public class AddAddressResponse
+    {
+        public bool Result { get; set; }
+    }
+}

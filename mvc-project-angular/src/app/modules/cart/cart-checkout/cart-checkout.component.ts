dba7f2934d42ddd2310ec32fc68bef89722e79d2
lukasz47 <77518@student.pb.edu.pl>
2021-12-16 20:23:50 +0100

    add swiper component

--- mvc-project-angular/src/app/modules/cart/cart-checkout/cart-checkout.component.ts
+++ mvc-project-angular/src/app/modules/cart/cart-checkout/cart-checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AddressService } from 'src/app/core/address/address.service';
+import { UserService } from 'src/app/core/user/user.service';
+import { Address } from 'src/app/shared/models/address';
 
 @Component({
   selector: 'app-cart-checkout',
@@ -7,13 +10,16 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class CartCheckoutComponent implements OnInit {
-
+  addresses: Address[] = []
   addAddress: boolean;
-  constructor() {
+  constructor(private addressService: AddressService, private userService: UserService) {
     this.addAddress = false;
   }
 
   ngOnInit(): void {
+    this.addressService.getByUserId(this.userService.getUserId()).subscribe(result => {
+      this.addresses = result.addresses
+    });
   }
 
   showAddAddress() {

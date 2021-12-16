8339a0e59e1d088bd0b8b6093d4dbe7e81598d26
Paweł Wiszniewski <pawel.wiszniewski20@gmail.com>
2021-12-16 10:58:10 +0100

    Modify register request, add automapper in user service

--- MVC_Project_Api/MVC_Project.Logic/Admin/MappingProfiles/UserProfile.cs
+++ MVC_Project_Api/MVC_Project.Logic/Admin/MappingProfiles/UserProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MVC_Project.Domain.Entities;
 using MVC_Project.Logic.Admin.Responses;
+using MVC_Project.Logic.Global.Requests;
 using System.Collections.Generic;
 
 namespace MVC_Project.Logic.Admin.MappingProfiles
@@ -19,7 +20,15 @@ namespace MVC_Project.Logic.Admin.MappingProfiles
 
             CreateMap<User, AdminGetUserById>()
                 .ForMember(dest => dest.UserId, opt =>
-opt.MapFrom(src => src.Id));
+                   opt.MapFrom(src => src.Id));
+
+            CreateMap<RegisterRequest, User>()
+                .ForMember(dest => dest.ThemeId, opt =>
+                   opt.MapFrom(src => 1))
+                .ForMember(dest => dest.ProductOnPageCount, opt =>
+                   opt.MapFrom(src => 0))
+                .ForMember(dest => dest.Provider, opt =>
+                   opt.MapFrom(src => 1));
         }
     }
 }

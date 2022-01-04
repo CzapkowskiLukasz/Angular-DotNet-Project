5745f38d9e0b665c4e7a69951ac48e3637521657
Paweł Wiszniewski <pawel.wiszniewski20@gmail.com>
2022-01-04 20:14:57 +0100

    Add get cart in cart checkout component

--- mvc-project-angular/src/app/modules/cart/cart-checkout/cart-checkout.component.ts
+++ mvc-project-angular/src/app/modules/cart/cart-checkout/cart-checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ProductService } from 'src/app/core/product/product.service';
 import { AddressService } from 'src/app/core/address/address.service';
 import { UserService } from 'src/app/core/user/user.service';
 import { Address } from 'src/app/shared/models/address';
@@ -12,12 +13,19 @@ import { Address } from 'src/app/shared/models/address';
 export class CartCheckoutComponent implements OnInit {
   addresses: Address[] = []
   addAddress: boolean;
-  user
-  constructor(private addressService: AddressService, private userService: UserService) {
+
+  products:[]=[];
+
+      constructor(private addressService: AddressService,
+          private userService: UserService,
+private productService: ProductService      ) {
     this.addAddress = false;
   }
 
   ngOnInit(): void {
+    this.productService.getProductByOrder(1).subscribe(result=>{
+      this.products=result.products;
+    })
     this.addressService.getByUserId(this.userService.getUserId()).subscribe(result => {
       this.addresses = result.addresses
     });

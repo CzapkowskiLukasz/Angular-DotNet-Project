30510d378cf1b77398b7c0a327394bb7f6e70071
Paweł Wiszniewski <pawel.wiszniewski20@gmail.com>
2022-01-04 20:13:04 +0100

    Add get user cart response and mapping

---
+++ MVC_Project_Api/MVC_Project.Logic/Customer/Responses/GetUserCartResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace MVC_Project.Logic.Customer.Responses
+{
+    public class GetUserCartResponse
+    {
+        public int Products { get; set; }
+    }
+}
---
+++ MVC_Project_Api/MVC_Project.Logic/Customer/Responses/UserCartListItem.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_Project.Logic.Customer.Responses
+{
+    public class UserCartListItem
+    {
+        public string ProductName { get; set; }
+        public string Count { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}

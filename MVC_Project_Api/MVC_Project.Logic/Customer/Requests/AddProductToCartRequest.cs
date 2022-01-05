a8167b50f93cc0534ce40d5b69d70ec3ce5a05d6
Paweł Wiszniewski <pawel.wiszniewski20@gmail.com>
2022-01-05 22:37:03 +0100

    Add AddProductToCart request and response

---
+++ MVC_Project_Api/MVC_Project.Logic/Customer/Requests/AddProductToCartRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_Project.Logic.Customer.Requests
+{
+    public class AddProductToCartRequest
+    {
+        public int ProductId { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ MVC_Project_Api/MVC_Project.Logic/Customer/Responses/AddProductToCartResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace MVC_Project.Logic.Customer.Responses
+{
+    public class AddProductToCartResponse
+    {
+        public bool Result { get; set; }
+    }
+}

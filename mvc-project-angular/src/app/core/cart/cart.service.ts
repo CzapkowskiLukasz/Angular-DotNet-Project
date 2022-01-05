ebe9e61bae1ab75ae8017a2af9cef18ac75b25c2
Paweł Wiszniewski <pawel.wiszniewski20@gmail.com>
2022-01-06 00:54:24 +0100

    Changed method for adding products in angular cart service

--- mvc-project-angular/src/app/core/cart/cart.service.ts
+++ mvc-project-angular/src/app/core/cart/cart.service.ts
@@ -1,18 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { ChangeProductCartCountRequest } from 'src/app/shared/models/requests/changeProductCartCountRequest';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  baseUrl = `${environment.api_url}/customer/cart/change-product-count`;
+  baseUrl = `${environment.api_url}/customer/cart`;
 
   constructor(private http: HttpClient) { }
 
-  changeProductCount(request): Observable<any> {
-    return this.http.post<any>(this.baseUrl, request);
+  addProductToCart(request: ChangeProductCartCountRequest): Observable<boolean> {
+    return this.http.post<any>(this.baseUrl + '/add-product', request).pipe(
+      map((val) => val.result)
+    );
   }
 }
--- mvc-project-angular/src/app/shared/models/requests/changeProductCartCountRequest.ts
+++ mvc-project-angular/src/app/shared/models/requests/changeProductCartCountRequest.ts
@@ -1,4 +1,4 @@
-export interface addProductToCartRequest {
+export interface ChangeProductCartCountRequest {
     productId: number,
     count: number
 }

a2195b7e9086df998cbea5019470e141ed97ac6e
Paweł Wiszniewski <pawel.wiszniewski20@gmail.com>
2022-01-09 20:17:28 +0100

    Moved cart endpoints to customer cart controller

---
+++ MVC_Project_Api/MVC_Project/Controllers/Customer/CustomerCartController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Project.Logic.Customer.Interfaces;
+using MVC_Project.Logic.Customer.Requests;
+using System.Threading.Tasks;
+
+namespace MVC_Project.Api.Controllers.Customer
+{
+    [ApiController]
+    [Route("customer/cart")]
+    public class CustomerCartController : ControllerBase
+    {
+        private readonly ICartService _cartService;
+
+        public CustomerCartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [HttpPost("change-product-count")]
+        public async Task<IActionResult> ChangeProductCartCountAsync([FromBody] ChangeProductCartCountRequest request)
+        {
+            var result = await _cartService.ChangeProductCartCountAsync(request);
+
+            if (result.ErrorResponse == null)
+            {
+                return Ok(result.Response);
+            }
+
+            return StatusCode(result.ErrorResponse.ErrorCode, result.ErrorResponse);
+        }
+
+        [HttpGet("user-cart")]
+        public async Task<IActionResult> GetUserCartAsync()
+        {
+            var result = await _cartService.GetUserCartAsync();
+
+            return Ok(result.Response);
+        }
+
+        [HttpPost("add-product")]
+        public async Task<IActionResult> AddProductToCartAsync([FromBody] AddProductToCartRequest request)
+        {
+            var result = await _cartService.AddProductToCartAsync(request);
+
+            if (result.ErrorResponse == null)
+            {
+                return Ok(result.Response);
+            }
+
+            return StatusCode(result.ErrorResponse.ErrorCode, result.ErrorResponse);
+        }
+    }
+}

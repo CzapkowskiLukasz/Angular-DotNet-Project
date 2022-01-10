7116fac4815b8bda990c853918a8e4ab32309db2
Paweł Wiszniewski <pawel.wiszniewski20@gmail.com>
2022-01-10 23:23:06 +0100

    Added waiting for multiple click during modification product count

--- mvc-project-angular/src/app/shared/modules/cart-element/cart-element.component.ts
+++ mvc-project-angular/src/app/shared/modules/cart-element/cart-element.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { CartService } from 'src/app/core/cart/cart.service';
 import { ComponentConnectionService } from 'src/app/core/componentConnection/component-connection.service';
 import { CartItem } from '../../models/cart-item';
@@ -10,38 +12,67 @@ import { ChangeProductCartCountRequest } from '../../models/requests/changeProdu
   styleUrls: ['./cart-element.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class CartElementComponent implements OnInit {
+export class CartElementComponent implements OnInit, OnDestroy {
 
   @Input() item: CartItem;
+
+  valueSubject: Subject<void>;
+
+  valueSubscribtion: Subscription;
+
+  changeCountValue: number = 0;
 
   constructor(private cartService: CartService,
     private componentConnection: ComponentConnectionService) { }
 
   ngOnInit(): void {
+    this.valueSubject = new Subject();
+
+    this.setSubscribtion();
+  }
+
+  ngOnDestroy(): void {
+    if (this.valueSubject) {
+      this.valueSubscribtion.unsubscribe();
+    }
   }
 
   addOneProduct() {
-    this.changeCount(1);
+    this.setValue(1);
   }
 
   subtractOneProduct() {
-    this.changeCount(-1);
+    this.setValue(-1);
   }
 
-  removeProduct(){
-    this.changeCount(-this.item.count);
+  removeProduct() {
+    this.setValue(-this.item.count);
   }
 
-  changeCount(count: number) {
+  private setValue(count: number) {
+    this.changeCountValue += count;
+
+    this.valueSubject.next();
+  }
+
+  private setSubscribtion() {
+    this.valueSubscribtion = this.valueSubject
+      .pipe(debounceTime(1000))
+      .subscribe(() => this.changeCount());
+  }
+
+  private changeCount() {
+    this.valueSubject.complete();
+
     const request: ChangeProductCartCountRequest = {
       productId: this.item.productId,
-      count: count
+      count: this.changeCountValue
     };
 
     this.cartService.changeProductCartCount(request).subscribe(result => {
       if (result) {
         this.componentConnection.sendCommand('refreshCart');
       }
-    })
+    });
   }
 }

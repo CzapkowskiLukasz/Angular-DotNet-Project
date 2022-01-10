043a0938d99b40821f6c37791317a50f7ba36f70
lukasz47 <77518@student.pb.edu.pl>
2022-01-10 18:59:09 +0100

    HTML tag work in products descriptions

--- MVC_Project_Api/MVC_Project.Logic/Warehouse/Responses/GetProductByIdResponse.cs
+++ MVC_Project_Api/MVC_Project.Logic/Warehouse/Responses/GetProductByIdResponse.cs
@@ -8,6 +8,8 @@
 
         public decimal Price { get; set; }
 
+        public string Description { get; set; }
+
         public int WarehouseQuantity { get; set; }
 
         public string CategoryName { get; set; }
